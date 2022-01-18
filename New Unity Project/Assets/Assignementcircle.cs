b189e68be30e9dc8a0c0079bbba2300b2a47ef99
Erikjohannesson01 <erijoh0517@skola.goteborg.se>
2022-01-18 15:59:50 +0100

    circle

---
+++ New Unity Project/Assets/Assignementcircle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignementcircle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float turning = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turning += Time.time;
+
+        float x = Mathf.Cos(turning);
+        float y = Mathf.Sign(turning);
+
+        transform.position = new Vector2(x, y);
+    }
+}
--- New Unity Project/Assets/Assignment1.cs
+++ New Unity Project/Assets/Assignment1.cs
@@ -6,25 +6,40 @@ public class Assignment1 : MonoBehaviour
 {
     public GameObject Cirkel;
     public GameObject Cirkel2;
+    public GameObject Cirkel3;
+
+
+    public float cc;
 
     void Start()
     {
         Spawn();
     }
 
+    private void Update()
+    {
+        spawnchange();
+    }
 
 
     void Spawn()
     {
-        float x = -10;
-        for (int i = 0; i < 100; i++)
+        
+        for (float i = -30; i < 100; i += 0.3f)
         {
-            Instantiate(Cirkel, new Vector2(x += 0.3f, 0), Quaternion.identity);
+            Instantiate(Cirkel, new Vector2(i -1, 0), Quaternion.identity);
+            Instantiate(Cirkel2, new Vector2(i -1, 0), Quaternion.identity);
         }
-        float e = -10;
-        for (int i = 0; i < 100; i++)
+
+       
+    }
+    void spawnchange()
+    {
+        for (float x = 0; x < Mathf.PI * 2; x += 0.1f)
         {
-            Instantiate(Cirkel2, new Vector2(e += 0.3f, 0), Quaternion.identity);
+
+            Instantiate(Cirkel3, new Vector2(Mathf.Cos(x) * cc, Mathf.Sin(x) * cc), Quaternion.identity);
         }
+
     }
 }
